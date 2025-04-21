83ece20c3ac56ea8ba859727b972fff7dab2945d
MONISH PATALAY <106901576+monishpatalay@users.noreply.github.com>
2025-04-20 19:27:42 -0700

    Update LoginPage.jsx

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -15,7 +15,7 @@ export default function LoginPage() {
     ev.preventDefault();
     // Make sure to call the correct URL (note the port).
     try{
-        const {data} = await axios.post("http://localhost:3000/login", { email, password });
+        const {data} = await axios.post("https://booking-app-fdsm.onrender.com/login", { email, password });
         setUser(data);
         alert("User logged in successfully");
         setRedirect(true);
