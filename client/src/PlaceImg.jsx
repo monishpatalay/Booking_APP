c91467853545be701b480b753f3b125fc53ac35a
MONISH PATALAY <106901576+monishpatalay@users.noreply.github.com>
2025-04-21 09:58:13 -0700

    Update PlaceImg.jsx

--- client/src/PlaceImg.jsx
+++ client/src/PlaceImg.jsx
@@ -4,7 +4,7 @@ export default function PlaceImg({ place, index = 0, className = "rounded-2xl ob
   return (
     <img
       className={className}
-      src={`https://booking-app-fdsm.onrender.com/uploads/${place.photos[index]}`}
+      src={`https://booking-app-0tpe.onrender.com/uploads/${place.photos[index]}`}
       alt={place.title}
     />
   );
