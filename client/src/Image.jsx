b16b821c74d5b64d660dd85f5b86ba2e2d815fd1
MONISH PATALAY <106901576+monishpatalay@users.noreply.github.com>
2025-04-21 13:29:47 -0700

    Update Image.jsx

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,7 +1,7 @@
 export default function Image({src,...rest}) {
     src = src && src.includes('https://')
       ? src
-      : 'https://booking-app-0tpe.onrender.com/uploads/'+src;
+      : 'https://booking-app-nbih.onrender.com/uploads/'+src;
     return (
       <img {...rest} src={src} alt={''} />
     );
