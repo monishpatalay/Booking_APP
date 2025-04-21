f348646b0eafe071c334dde8d174f26fe6122094
MONISH PATALAY <106901576+monishpatalay@users.noreply.github.com>
2025-04-20 19:20:15 -0700

    Update Image.jsx

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,7 +1,7 @@
 export default function Image({src,...rest}) {
     src = src && src.includes('https://')
       ? src
-      : 'http://localhost:3000/uploads/'+src;
+      : 'https://booking-app-fdsm.onrender.com/uploads/'+src;
     return (
       <img {...rest} src={src} alt={''} />
     );
